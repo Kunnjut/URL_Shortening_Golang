6dbdc51774b9e85f209c36eb21f45e6f53a45370
Roman Romanov <kunnjut@icloud.com>
2024-08-02 21:48:01 +0300

    New -> client

---
+++ cmd/client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	endpoint := "http://localhost:8080/"
+	fmt.Println("Введите длинный URL")
+	reader := bufio.NewReader(os.Stdin)
+	long, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	long = strings.TrimSuffix(long, "\n")
+	client := &http.Client{}
+	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(long))
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Add("Content-Type", "text/plain; charset=utf-8")
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Status Code ", response.Status)
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+}
